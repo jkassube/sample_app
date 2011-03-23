06072e99c88da49cec6e8d4ebea9b5c427420971
Justin Kassube <justin.kassube@gmail.com>
2011-03-23 18:54:28 -0500

    Still having trouble with email regexes, figure it out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,23 @@ describe User do
   it "should reject names that are too long" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name))
-    long_name.length.should == 51
+    long_name_user.should_not be_valid
+  end
+  
+  it "should accept valid email addresses" do
+    address = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    address.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+  
+  it "should reject invalid email adresses" do
+    address = %w[user@foo,com user_at_foo.org example.user@foo]
+    address.each do |address|
+      inavlid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
   end
   
 end
