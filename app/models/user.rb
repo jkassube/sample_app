d960b0ad64149f40b6950273556b67e614e7d58a
Justin Kassube <justin.kassube@gmail.com>
2011-03-24 17:30:57 -0500

    Chapter 7.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,17 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
   
-  validates :name,  :presence   => true,
-                    :length     => { :maximum => 50 }
-  validates :email, :presence   => true,
-                    :format     => { :with => email_regex },
-                    :uniqueness => { :case_sensitive => false }
+  validates :password,  :presence     => true,
+                        :confirmation => true,
+                        :length       => { :within => 6..40 }
+  validates :name,      :presence     => true,
+                        :length       => { :maximum => 50 }
+  validates :email,     :presence     => true,
+                        :format       => { :with => email_regex },
+                        :uniqueness   => { :case_sensitive => false }
 end
